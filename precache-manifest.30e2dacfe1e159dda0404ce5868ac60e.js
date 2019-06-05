0c07fc668216b7e59ead727406ec5da3742a4580
Steven Williams <stevenwritescode@gmail.com>
2019-06-04 21:18:59 -0600

    Updates

---
+++ precache-manifest.30e2dacfe1e159dda0404ce5868ac60e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd94e56dbb7876c0a2d5075ca80624f",
+    "url": "/very-basic-kanban-board/index.html"
+  },
+  {
+    "revision": "0dbb22d2cfc66a6a4221",
+    "url": "/very-basic-kanban-board/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f086a987c6f7c57b3a2d",
+    "url": "/very-basic-kanban-board/static/js/2.dba58bda.chunk.js"
+  },
+  {
+    "revision": "0dbb22d2cfc66a6a4221",
+    "url": "/very-basic-kanban-board/static/js/main.f58bc1dc.chunk.js"
+  },
+  {
+    "revision": "d6cd11ebb7c915b80a77",
+    "url": "/very-basic-kanban-board/static/js/runtime~main.6a79fffa.js"
+  }
+]);
